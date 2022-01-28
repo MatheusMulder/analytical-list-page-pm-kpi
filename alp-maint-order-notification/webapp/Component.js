d2c1f0f26814fe597a2691799e4570785c77cbe8
MatheusMulder <matheusjohanmulder@gmail.com>
2022-01-28 13:57:19 -0300

    ALP - PM/KPI
    ALP - PM/KPI

---
+++ alp-maint-order-notification/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("sap.pm.custom.fiorielements.alpmaintordernotification.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
